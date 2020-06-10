fe6f3c0fb7153c2aac35f382cb005f6cbf276ad2
Tim Nilimaa-Svärd <tim.nilimaa-svard@jag.se>
2020-06-10 20:05:36 +0200

    Changed to not use a private method

--- src/client/DreambrokerClient.cs
+++ src/client/DreambrokerClient.cs
@@ -12,11 +12,6 @@ namespace JAG.Dreambroker
         public static async Task<JsonResponse> GetFeedDataAsync(Uri url)
         {
             if (url == null) throw new ArgumentNullException($"{nameof(url)} must not be null.");
-            return await getFeedDataAsync(url).ConfigureAwait(false);
-        }
-
-        private static async Task<JsonResponse> getFeedDataAsync(Uri url)
-        {
             using (var httpClient = new HttpClient())
             {
                 var responseMessage = await httpClient.GetAsync(url)
